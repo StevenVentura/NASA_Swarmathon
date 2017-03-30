ba97d5d1e59c4ef57fff5dfdab5afadec7ae8f39
Steven <8stevenventura@gmail.com>
2017-03-29 19:26:45 -0500

    remove ignored files

--- build/rqt_rover_gui/catkin_generated/ordered_paths.cmake
+++
@@ -1,1 +0,0 @@
-set(ORDERED_PATHS "/home/steven/rover_workspace/devel/lib;/opt/ros/indigo/lib")
--- build/ublox_msgs/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/steven/rover_workspace/install/include".split(';') if "/home/steven/rover_workspace/install/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "message_runtime;ublox_serialization;std_msgs;sensor_msgs".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lublox_msgs".split(';') if "-lublox_msgs" != "" else []
-PROJECT_NAME = "ublox_msgs"
-PROJECT_SPACE_DIR = "/home/steven/rover_workspace/install"
-PROJECT_VERSION = "0.0.0"
